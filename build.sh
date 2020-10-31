3dfbcaba2f9e2da79a9fb4cebcd26d7edad4034a
anhhuu <panhhuu@gmail.com>
2020-10-31 21:45:58 +0700

    delete temp file and update buld script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 rm build/history.o
-rm build/excution.o
+rm build/execution.o
 rm build/parsing.o
 rm build/utils.o
 rm build/main.o
@@ -9,9 +9,9 @@ mkdir bin
 mkdir build
 
 gcc src/history.c -o build/history.o -c
-gcc src/execution.c -o build/excution.o -c
+gcc src/execution.c -o build/execution.o -c
 gcc src/parsing.c -o build/parsing.o -c
 gcc src/utils.c -o build/utils.o -c
 gcc src/main.c -o build/main.o -c
 
-gcc -o bin/osh build/history.o build/excution.o build/parsing.o build/utils.o build/main.o
+gcc -o bin/osh build/history.o build/execution.o build/parsing.o build/utils.o build/main.o
